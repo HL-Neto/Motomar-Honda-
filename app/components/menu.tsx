cafbdfb47612e3ec23580ab455b1312da4d3a7f7
HL-Neto <humbertolunaneto@gmail.com>
2026-01-29 20:02:22 -0300

    dev vers 2

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import styles from "@/app/styles/menu.module.css"
 
 import logo from "@/app/assets/logohero_motomar.png"
-
+import honda from "@/app/assets/honda_logo.png"
 
 
 
@@ -12,23 +12,27 @@ export default function Hero(){
         
         <div className={styles.menu}>
             
-
+           
             <nav>
                 <ul>
-                    <li><Image className="  w-[190px] md:w-[250px] h-[190px]"
-                        src={logo}
-                        alt="logo" 
+                    <li><Image className="   md:w-[90px] md:h-[100px]"
+                        src={honda}
+                        alt="honda_logo" 
                      
                         />
                     </li>
                 </ul>
             </nav>
-
-
-      
-
-
-
+            <nav>
+                <ul>
+                    <li><Image className="  w-[190px] md:w-[250px] h-[190px]"
+                        src={logo}
+                        alt="logo" 
+                        />
+                    </li>
+                </ul>
+            </nav>
+           
             <div>
                 <nav >
                     <ul className={styles.nav}>
@@ -67,43 +71,33 @@ export default function Hero(){
                                 href="duplachance">Dupla Chance
                             </a>
                         </li>
-                        
-                        <li>
-                            <a 
-                                href="https://www.honda.com.br/motos/honda-store/"  
-                                target="_blank"
-                                rel="noopener noreferrer">Honda Store
-                            </a>
+
+                        <li className={styles.dropdown}>
+                            <button type="button" className={styles.dropdownTrigger} aria-expanded="false" aria-haspopup="true">
+                                Serviços Honda
+                                <span className={styles.arrow} aria-hidden="true">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.5 4.5L6 8L9.5 4.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </span>
+                            </button>
+                            <ul className={styles.dropdownMenu}>
+                                <li>
+                                    <a href="https://www.honda.com.br/motos/honda-store/" target="_blank" rel="noopener noreferrer">Honda Store</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.honda.com.br/pecas/motos/" target="_blank" rel="noopener noreferrer">Peças e Acessórios</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.honda.com.br/recall" target="_blank" rel="noopener noreferrer">Recall</a>
+                                </li>
+                            </ul>
                         </li>
 
-                        <li>
-                            <a
-                                href="https://www.honda.com.br/pecas/motos/"
-                                target="_blank"
-                                rel="noopener noreferrer">Peças/Acessórios
-                            </a>
-                        </li>
-
-                        <li>
-                            <a 
-                                href="https://api.whatsapp.com/send?phone=558330484400&text=Ol%C3%
-                                A1!%20Tenho%20interesse%20neste%20produto.%20%23chave%3A%20a324M0000
-                                00e0qfQAA"
-                                target="_blank"
-                                rel="noopener noreferrer">Contato 
-                            </a>
-                        </li>
-                        
-                        <li>
-                            <a 
-                                href="https://www.honda.com.br/recall"
-                                target="_blank"
-                                rel="noopener noreferrer">Recall
-                            </a>
-                        </li>                   
-                    </ul>
+                     </ul>
                 </nav> 
             </div>
+           
            
             
           
